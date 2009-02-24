b640211414a571c7dedeba00155c282258e89c94
Maciej Bilas <maciej@inszy.org>
2009-02-24 16:27:22 +0100

    JavaNative2Ascii.native2ascii performance improved.

--- lib/java_native2ascii.rb
+++ lib/java_native2ascii.rb
@@ -14,24 +14,27 @@ class JavaNative2Ascii
   
   def self.native2ascii str
     out = ""
-    str = Iconv.conv("UNICODE", "UTF-8", str)
-    even, last, i = false, 0, 0
-    str.each_byte do |b|
-      i+=1
-      next if i < 3 # skip 2 first characters
-      even = !even
-      if even
-        last = b
-        next
-      end
-      
-      if (last > 127 || b > 0)
-        out << sprintf("\\u%04x", (b << 8 | last))
+
+    arr = str.unpack("U*")
+    return out if arr.nil?
+    #    arr_s = arr.size
+    #    i = 0
+    #
+    #    while i < arr_s
+    #      c = arr[i]
+    #      if c > 127
+    #        out << sprintf("\\u%04x", c)
+    #      else
+    #        out << c
+    #      end
+    #      i+=1
+    #    end
+    arr.each do |c|
+      if c > 127
+        out << sprintf("\\u%04x", c)
       else
-        out << last
+        out << c
       end
-      
-      last = b
     end
     out
   end
