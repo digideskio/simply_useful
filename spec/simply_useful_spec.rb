d5b1c2e36137a737562fa190d74555adc3eb8f74
Maciej Bilas <maciej@inszy.org>
2010-03-19 13:12:17 +0100

    Test if module load (at least some).

--- spec/simply_useful_spec.rb
+++ spec/simply_useful_spec.rb
@@ -5,4 +5,14 @@ describe SimplyUseful do
   it "should load" do
     # if it gets here then it means no errors were raised during loading of the module (probably, right?)
   end
+
+  describe "included modules" do
+    
+    it "should define Hash.deep_symbolize_keys!" do
+      lambda { {"a" => "b"}.deep_symbolize_keys! }.should_not raise_error
+    end
+    
+  end
+
+  
 end
