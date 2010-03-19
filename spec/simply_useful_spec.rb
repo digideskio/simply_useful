f8c98036ef3ba19f1eadef62c9b7c28cde45fbce
Maciej Bilas <maciej@inszy.org>
2010-03-19 12:48:36 +0100

    Added simple test to check if module loads.

---
+++ spec/simply_useful_spec.rb
@@ -0,0 +1,8 @@
+require File.dirname(__FILE__) + '/spec_helper'
+require 'simply_useful'
+
+describe SimplyUseful do
+  it "should load" do
+    # if it gets here then it means no errors were raised during loading of the module (probably, right?)
+  end
+end
